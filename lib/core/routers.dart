999c6a1e59089269e887e49802c621de499f1db6
Chidiebube Iroezindu <chidiebubeiroezindu@gmail.com>
2025-05-08 18:27:21 +0100

    Add Dashboard screen and update router configuration to include it

--- lib/core/routers.dart
+++ lib/core/routers.dart
@@ -1,5 +1,6 @@
 import 'package:d_crypto_lite/core/routes.dart';
 import 'package:d_crypto_lite/presentation/screens/auth_interface.dart';
+import 'package:d_crypto_lite/presentation/screens/dashboard.dart';
 import 'package:d_crypto_lite/presentation/screens/login_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,6 +10,7 @@ class Routers {
     routes: [
       GoRoute(path: Routes.authInterface, builder: (context, state) => const AuthInterface()),
       GoRoute(path: Routes.loginScreen, builder: (context, state) => const LoginScreen()),
+      GoRoute(path: Routes.dashboard, builder: (context, state) => const Dashboard()),
     ],
   );
 }
